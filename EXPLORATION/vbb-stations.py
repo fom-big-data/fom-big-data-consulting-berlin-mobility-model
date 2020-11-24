0d98eaa2fac8a48de3a94a55af0944d7f8264581
Florian Schwanz <florian.schwanz@interoberlin.de>
2020-11-24 21:08:47 +0100

    Transform VBB stations into GeoJSON #11

---
+++ EXPLORATION/vbb-stations.py
@@ -0,0 +1,22 @@
+import csv
+
+from geojson import Feature, FeatureCollection, Point
+
+features = []
+with open('../data/vbb-track-gtfs/extracted/stops.txt', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',', )
+    next(reader)
+
+    for stop_id, stop_code, stop_name, stop_desc, stop_lat, stop_lon, location_type, parent_station, wheelchair_boarding, platform_code, zone_id in reader:
+        features.append(
+            Feature(
+                geometry=Point((float(stop_lon), float(stop_lat))),
+                properties={
+                    'stop-name': stop_name
+                }
+            )
+        )
+
+collection = FeatureCollection(features)
+with open('../results/vbb-stations.geojson', 'w') as f:
+    f.write('%s' % collection)
