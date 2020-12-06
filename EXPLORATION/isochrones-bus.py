9fda9c4a453c5e0b210600fa27fb5ec966e72723
Florian Schwanz <florian.schwanz@interoberlin.de>
2020-12-06 14:23:08 +0100

    Use local graphml caching, combine bus and walk net, track failed points #14

--- EXPLORATION/isochrones-bus.py
+++ EXPLORATION/isochrones-bus.py
@@ -1,27 +1,26 @@
 import csv
 from os import path
-from tqdm import tqdm
+
 import geopy.distance
 import networkx as nx
 import numpy as np
 import osmnx as ox
 from geojson import FeatureCollection
 from shapely.geometry import MultiPoint
-from shapely.geometry import LineString
+from tqdm import tqdm
 
 
-def download_graphml(place_name, custom_filter, filepath):
-    print("download_graphmal")
+def load_graphml_from_file(filepath, place_name, network_type=None, custom_filter=None):
     if not path.exists(filepath):
-        graph = load_graphml(place_name, custom_filter)
+        graph = load_graphml(place_name, network_type=network_type, custom_filter=custom_filter)
         ox.save_graphml(graph, filepath=filepath)
-        print("download finished")
     else:
-        print("download unnecessary")
+        return ox.io.load_graphml(filepath=filepath)
 
 
-def load_graphml(place_name, custom_filter):
-    return ox.graph.graph_from_place(place_name, retain_all=True, buffer_dist=2500, custom_filter=custom_filter)
+def load_graphml(place_name, network_type=None, custom_filter=None):
+    return ox.graph.graph_from_place(place_name, retain_all=True, buffer_dist=2500, network_type=network_type,
+                                     custom_filter=custom_filter)
 
 
 def load_sample_points(file_path):
@@ -38,18 +37,16 @@ def load_sample_points(file_path):
 
 def get_points_with_spatial_distance(g, points, travel_time_min):
     points_with_spatial_distance = []
+    failed_points = []
 
     progress_bar = tqdm(iterable=range(len(points)), unit='points', desc="Evaluate points")
     for point_index in progress_bar:
-
         point = points[point_index]
         start_point = (float(point["lat"]), float(point["lon"]))
         mean_spatial_distance = get_mean_spatial_distance(g=g,
                                                           start_point=start_point,
                                                           travel_time_min=travel_time_min,
                                                           real_speed=True)
-
-        mean_spatial_distances.append(mean_spatial_distance)
 
         nearest_node_id = ox.get_nearest_node(g, start_point)
         nearest_node = g.nodes[nearest_node_id]
@@ -60,9 +57,13 @@ def get_points_with_spatial_distance(g, points, travel_time_min):
             "mean_spatial_distance_15min": mean_spatial_distance,
         }
 
-        points_with_spatial_distance.append(point_with_spatial_distance)
+        if mean_spatial_distance > 0:
+            mean_spatial_distances.append(mean_spatial_distance)
+            points_with_spatial_distance.append(point_with_spatial_distance)
+        else:
+            failed_points.append(point_with_spatial_distance)
 
-    return points_with_spatial_distance
+    return points_with_spatial_distance, failed_points
 
 
 def get_mean_spatial_distance(g, start_point, travel_time_min, real_speed=True):
@@ -84,8 +85,9 @@ def get_possible_routes(g, start_point, travel_time_min, real_speed=True):
     subgraph = nx.ego_graph(g, center_node, radius=travel_time_min, distance=distance_type)
     return ox.graph_to_gdfs(subgraph)
 
+
 def get_convex_hull(nodes):
-    return MultiPoint(nodes.reset_index()['geometry']).convex_hull.coords.xy
+    return MultiPoint(nodes.reset_index()['geometry']).convex_hull.exterior.coords.xy
 
 
 def get_distances(start_point, latitudes, longitudes):
@@ -116,16 +118,20 @@ TRAVEL_TIME_MIN = 15
 
 mean_spatial_distances = []
 
-# Load bus graph
-g = load_graphml(PLACE_NAME, '["bus"="yes"]')
+# Load and compose bus and walk graphs
+gBus = load_graphml_from_file('tmp/bus.graphml', PLACE_NAME, custom_filter='["bus"]')
+gWalk = load_graphml_from_file('tmp/walk.graphml', PLACE_NAME, network_type='walk')
+g = nx.algorithms.operators.all.compose_all([gBus, gWalk])
 
 # Load sample points
 sample_points = load_sample_points("../results/sample-points.csv")
 
-points_with_spatial_distance = get_points_with_spatial_distance(g, sample_points[:5000], TRAVEL_TIME_MIN)
+# Generate points
+points_with_spatial_distance, failed_points = get_points_with_spatial_distance(g, sample_points, TRAVEL_TIME_MIN)
 
 # Write coords to file
 write_coords_to_geojson(points_with_spatial_distance, '../results/isochrones-bus.geojson')
+write_coords_to_geojson(failed_points, '../results/isochrones-bus-failed.geojson')
 
 print("Complete!")
 print("min distance " + str(min(mean_spatial_distances)))
