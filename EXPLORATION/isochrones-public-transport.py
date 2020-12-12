f0e276ff26e37e8ddf669b8efecb42d59f78ffbb
Florian Schwanz <florian.schwanz@interoberlin.de>
2020-12-12 09:01:50 +0100

    Append bike, subway and rail to list of means of transport #14

--- EXPLORATION/isochrones-public-transport.py
+++ EXPLORATION/isochrones-public-transport.py
@@ -10,26 +10,46 @@ from shapely.geometry import MultiPoint
 from tqdm import tqdm
 
 
-def load_graphml_from_file(filepath, place_name, network_type=None, custom_filter=None):
-    if not path.exists(filepath):
-        print("Download " + filepath)
-        graph = load_graphml(place_name, network_type=network_type, custom_filter=custom_filter)
-        ox.save_graphml(graph, filepath=filepath)
+def load_graphml_from_file(file_path, place_name, network_type=None, custom_filter=None):
+    if not path.exists(file_path):
+        print("Download " + file_path)
+        graph = load_graphml(place_name=place_name,
+                             network_type=network_type,
+                             custom_filter=custom_filter)
+        ox.save_graphml(graph, file_path)
         return graph
     else:
-        return ox.io.load_graphml(filepath=filepath)
+        return ox.io.load_graphml(file_path)
 
 
 def load_graphml(place_name, network_type=None, custom_filter=None):
-    return ox.graph.graph_from_place(place_name, retain_all=False, buffer_dist=2500, network_type=network_type,
+    return ox.graph.graph_from_place(place_name=place_name,
+                                     retain_all=False,
+                                     buffer_dist=2500,
+                                     network_type=network_type,
                                      custom_filter=custom_filter)
 
 
 def get_means_of_transport_graph(name):
+    if name == "all":
+        return nx.algorithms.operators.all.compose_all([get_means_of_transport_graph("bike"),
+                                                        get_means_of_transport_graph("bus"),
+                                                        get_means_of_transport_graph("subway"),
+                                                        get_means_of_transport_graph("tram"),
+                                                        get_means_of_transport_graph("rail"),
+                                                        ])
+    if name == "bike":
+        return load_graphml_from_file(file_path="tmp/" + name + ".graphml",
+                                      place_name=PLACE_NAME,
+                                      network_type='bike')
     if name == "bus":
-        return load_graphml_from_file("tmp/" + name + ".graphml", PLACE_NAME, custom_filter='["bus"]')
+        return load_graphml_from_file(file_path="tmp/" + name + ".graphml",
+                                      place_name=PLACE_NAME,
+                                      custom_filter='["bus"="yes"]')
     elif name == "subway" or name == "tram" or name == "rail":
-        return load_graphml_from_file("tmp/" + name + ".graphml", PLACE_NAME, custom_filter='["railway"~"' + name + '"]')
+        return load_graphml_from_file(file_path="tmp/" + name + ".graphml",
+                                      place_name=PLACE_NAME,
+                                      custom_filter='["railway"~"' + name + '"]')
 
 
 def load_sample_points(file_path):
@@ -131,22 +151,24 @@ def write_mean_spatial_distances_to_file(mean_spatial_distances, file_path):
 
 PLACE_NAME = "Berlin, Germany"
 TRAVEL_TIMES = [5, 10, 15]
-MEANS_OF_TRANSPORT = ["bus", "tram"]
+MEANS_OF_TRANSPORT = ["all", "bike", "bus", "subway", "tram", "rail"]
 
 # Load complete graph
-# g_all = load_graphml_from_file('tmp/all.graphml', PLACE_NAME, network_type='all')
+# g_all = load_graphml_from_file(file_path='tmp/all.graphml', place_name=PLACE_NAME, network_type='all')
 
 # Load walk graph
-g_walk = load_graphml_from_file('tmp/walk.graphml', PLACE_NAME, network_type='walk')
+g_walk = load_graphml_from_file(file_path='tmp/walk.graphml',
+                                place_name=PLACE_NAME,
+                                network_type='walk')
 
 # Load sample points
-sample_points = load_sample_points("../results/sample-points.csv")
+sample_points = load_sample_points(file_path="../results/sample-points.csv")
 
 # Iterate over means of transport
 for name in MEANS_OF_TRANSPORT:
 
     # Get graph for means of transport
-    g_transport = get_means_of_transport_graph(name)
+    g_transport = get_means_of_transport_graph(name=name)
 
     # Compose means of transport with walking
     g = nx.algorithms.operators.all.compose_all([g_transport, g_walk])
@@ -162,12 +184,19 @@ for name in MEANS_OF_TRANSPORT:
             # Generate points
             points_with_spatial_distance, \
             failed_points, \
-            mean_spatial_distances = get_points_with_spatial_distance(g, sample_points, travel_time_min)
+            mean_spatial_distances = get_points_with_spatial_distance(g=g,
+                                                                      points=sample_points,
+                                                                      travel_time_min=travel_time_min)
 
             # Write results to file
-            write_coords_to_geojson(points_with_spatial_distance, travel_time_min, result_file_name_base + ".geojson")
-            write_coords_to_geojson(failed_points, travel_time_min, result_file_name_base + "-failed.geojson")
-            write_mean_spatial_distances_to_file(mean_spatial_distances, result_file_name_base + "-distances.txt")
+            write_coords_to_geojson(coords=points_with_spatial_distance,
+                                    travel_time_min=travel_time_min,
+                                    file_path=result_file_name_base + ".geojson")
+            write_coords_to_geojson(coords=failed_points,
+                                    travel_time_min=travel_time_min,
+                                    file_path=result_file_name_base + "-failed.geojson")
+            write_mean_spatial_distances_to_file(mean_spatial_distances=mean_spatial_distances,
+                                                 file_path=result_file_name_base + "-distances.txt")
         else:
             print(">>> Exists " + name + " in " + str(travel_time_min) + " minutes")
 
