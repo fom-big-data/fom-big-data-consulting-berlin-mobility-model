6f10bd8b361c3973fbce652a40403db7b713b6ec
Florian Schwanz <florian.schwanz@interoberlin.de>
2020-12-16 01:36:38 +0100

    Take travel speeds into account #15

--- EXPLORATION/isochrones-public-transport.py
+++ EXPLORATION/isochrones-public-transport.py
@@ -30,24 +30,54 @@ def load_graphml(place_name, network_type=None, custom_filter=None):
                                      custom_filter=custom_filter)
 
 
-def get_means_of_transport_graph(name):
-    if name == "all":
+def get_means_of_transport_graph(transport):
+    if transport == "all":
         return nx.algorithms.operators.all.compose_all([get_means_of_transport_graph("bus"),
                                                         get_means_of_transport_graph("subway"),
                                                         get_means_of_transport_graph("tram"),
                                                         get_means_of_transport_graph("rail")])
-    if name == "bike":
-        return load_graphml_from_file(file_path="tmp/" + name + ".graphml",
+    if transport == "bike":
+        return load_graphml_from_file(file_path="tmp/" + transport + ".graphml",
                                       place_name=PLACE_NAME,
                                       network_type='bike')
-    if name == "bus":
-        return load_graphml_from_file(file_path="tmp/" + name + ".graphml",
+    if transport == "bus":
+        return load_graphml_from_file(file_path="tmp/" + transport + ".graphml",
                                       place_name=PLACE_NAME,
                                       custom_filter='["bus"="yes"]')
-    elif name == "subway" or name == "tram" or name == "rail":
-        return load_graphml_from_file(file_path="tmp/" + name + ".graphml",
+    elif transport == "subway" or transport == "tram" or transport == "rail":
+        return load_graphml_from_file(file_path="tmp/" + transport + ".graphml",
                                       place_name=PLACE_NAME,
-                                      custom_filter='["railway"~"' + name + '"]')
+                                      custom_filter='["railway"~"' + transport + '"]')
+
+
+def enhance_with_speed(g, time_attribute='time', transport=None):
+    for _, _, _, data in g.edges(data=True, keys=True):
+
+        if (transport == 'walk'):
+            speed = 6.0
+        elif (transport == 'bus'):
+            # if ('maxspeed' in data.keys()):
+            #     if (isinstance(data['maxspeed'], list)):
+            #         speed = min(data['maxspeed'])
+            #     else:
+            #         speed = data['maxspeed']
+            #         speed = str(speed).strip(' mph')
+            # else:
+            #     speed = 50.0
+            speed = 19.5
+        elif (transport == 'bike'):
+            speed = 16.0
+        elif (transport == 'subway'):
+            speed = 31.0
+        elif (transport == 'tram'):
+            speed = 19.0
+        elif (transport == 'rail'):
+            speed = 38.0
+
+        if speed is not None:
+            data[time_attribute] = data['length'] / (float(speed) * 1000 / 60)
+
+    return g
 
 
 def load_sample_points(file_path):
@@ -73,8 +103,7 @@ def get_points_with_spatial_distance(g, points, travel_time_min):
         start_point = (float(point["lat"]), float(point["lon"]))
         mean_spatial_distance = get_mean_spatial_distance(g=g,
                                                           start_point=start_point,
-                                                          travel_time_min=travel_time_min,
-                                                          real_speed=True)
+                                                          travel_time_min=travel_time_min)
 
         nearest_node_id = ox.get_nearest_node(g, start_point)
         nearest_node = g.nodes[nearest_node_id]
@@ -94,10 +123,10 @@ def get_points_with_spatial_distance(g, points, travel_time_min):
     return points_with_spatial_distance, failed_points, mean_spatial_distances
 
 
-def get_mean_spatial_distance(g, start_point, travel_time_min, real_speed=True):
+def get_mean_spatial_distance(g, start_point, travel_time_min, distance_attribute='time'):
     try:
         # print("OKAY " + str(start_point))
-        nodes, edges = get_possible_routes(g, start_point, travel_time_min, real_speed)
+        nodes, edges = get_possible_routes(g, start_point, travel_time_min, distance_attribute)
 
         longitudes, latitudes = get_convex_hull(nodes)
         distances = get_distances(start_point, latitudes, longitudes)
@@ -107,10 +136,9 @@ def get_mean_spatial_distance(g, start_point, travel_time_min, real_speed=True):
         return 0
 
 
-def get_possible_routes(g, start_point, travel_time_min, real_speed=True):
+def get_possible_routes(g, start_point, travel_time_min, distance_attribute):
     center_node = ox.get_nearest_node(g, start_point)
-    distance_type = "real_time" if real_speed else "time"
-    subgraph = nx.ego_graph(g, center_node, radius=travel_time_min, distance=distance_type)
+    subgraph = nx.ego_graph(g, center_node, radius=travel_time_min, distance=distance_attribute)
     return ox.graph_to_gdfs(subgraph)
 
 
@@ -148,8 +176,10 @@ def write_mean_spatial_distances_to_file(mean_spatial_distances, file_path):
 #
 
 PLACE_NAME = "Berlin, Germany"
-TRAVEL_TIMES = [5, 10, 15]
-MEANS_OF_TRANSPORT = ["all", "bike", "bus", "subway", "tram", "rail"]
+# TRAVEL_TIMES = [5, 10, 15]
+# MEANS_OF_TRANSPORT = ["all", "bike", "bus", "subway", "tram", "rail"]
+TRAVEL_TIMES = [5]
+MEANS_OF_TRANSPORT = ["tram"]
 
 # Load complete graph
 # g_all = load_graphml_from_file(file_path='tmp/all.graphml', place_name=PLACE_NAME, network_type='all')
@@ -159,14 +189,20 @@ g_walk = load_graphml_from_file(file_path='tmp/walk.graphml',
                                 place_name=PLACE_NAME,
                                 network_type='walk')
 
+# Enhance graph with speed
+g_walk = enhance_with_speed(g=g_walk, transport='walk')
+
 # Load sample points
 sample_points = load_sample_points(file_path="../results/sample-points.csv")
 
 # Iterate over means of transport
-for name in MEANS_OF_TRANSPORT:
+for transport in MEANS_OF_TRANSPORT:
 
     # Get graph for means of transport
-    g_transport = get_means_of_transport_graph(name=name)
+    g_transport = get_means_of_transport_graph(transport=transport)
+
+    # Enhance graph with speed
+    g_transport = enhance_with_speed(g=g_transport, transport=transport)
 
     # Compose means of transport with walking
     g = nx.algorithms.operators.all.compose_all([g_transport, g_walk])
@@ -174,10 +210,10 @@ for name in MEANS_OF_TRANSPORT:
     # Iterate over travel times
     for travel_time_min in TRAVEL_TIMES:
 
-        result_file_name_base = "../results/isochrones-" + name + "-" + str(travel_time_min)
+        result_file_name_base = "../results/isochrones-" + transport + "-" + str(travel_time_min)
 
         if not path.exists(result_file_name_base + ".geojson"):
-            print(">>> Analyze " + name + " in " + str(travel_time_min) + " minutes")
+            print(">>> Analyze " + transport + " in " + str(travel_time_min) + " minutes")
 
             # Generate points
             points_with_spatial_distance, \
@@ -196,6 +232,6 @@ for name in MEANS_OF_TRANSPORT:
             write_mean_spatial_distances_to_file(mean_spatial_distances=mean_spatial_distances,
                                                  file_path=result_file_name_base + "-distances.txt")
         else:
-            print(">>> Exists " + name + " in " + str(travel_time_min) + " minutes")
+            print(">>> Exists " + transport + " in " + str(travel_time_min) + " minutes")
 
 print("Complete!")
