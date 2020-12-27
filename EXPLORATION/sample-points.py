4146d960936815644fc4dec6957ee2d534d9a64b
Florian Schwanz <florian.schwanz@interoberlin.de>
2020-12-27 20:05:10 +0100

    Re-generate sample points #23

--- EXPLORATION/sample-points.py
+++ EXPLORATION/sample-points.py
@@ -32,14 +32,22 @@ def get_bounding_box(polygon):
     return env[0], env[2], env[1], env[3]
 
 
-def get_random_points_in_polygons(polygons, num_point):
+def get_random_points_in_polygons(polygons_districts,
+                                  polygons_cemetery,
+                                  polygons_forest,
+                                  polygons_garden,
+                                  polygons_park,
+                                  polygons_recreation_ground,
+                                  polygons_water,
+                                  polygons_wood,
+                                  num_point):
     points = []
     xmin = None
     ymin = None
     xmax = None
     ymax = None
 
-    for polygon in polygons:
+    for polygon in polygons_districts:
 
         # Get bounding box
         boundingXmin, boundingYmin, boundingXmax, boundingYmax = get_bounding_box(polygon)
@@ -53,22 +61,89 @@ def get_random_points_in_polygons(polygons, num_point):
         if ymax == None or boundingYmax > ymax:
             ymax = boundingYmax
 
-
     counter = 0
+    fail_counter = 0
     while counter < num_point:
 
         point = ogr.Geometry(ogr.wkbPoint)
         point.AddPoint(random.uniform(xmin, xmax),
                        random.uniform(ymin, ymax))
 
-        for polygon in polygons:
-            if point.Within(polygon):
-                points.append(point)
+        if is_in_desired_area(point,
+                       polygons_districts,
+                       polygons_cemetery,
+                       polygons_forest,
+                       polygons_garden,
+                       polygons_park,
+                       polygons_recreation_ground,
+                       polygons_water,
+                       polygons_wood):
 
-                counter += 1
+            points.append(point)
+            counter += 1
+            print(counter)
+        else:
+            fail_counter += 1
+
+    print("failed " + str(fail_counter))
 
     return points
 
+
+def is_in_desired_area(point,
+                       polygons_districts,
+                       polygons_cemetery,
+                       polygons_forest,
+                       polygons_garden,
+                       polygons_park,
+                       polygons_recreation_ground,
+                       polygons_water,
+                       polygons_wood):
+
+    in_district = False
+    for polygon in polygons_districts:
+        if point.Within(polygon):
+            in_district = True
+
+    if not in_district:
+        return False
+
+    for polygon in polygons_cemetery:
+        if point.Within(polygon):
+            print("in cemetery")
+            return False
+
+    for polygon in polygons_forest:
+        if point.Within(polygon):
+            print("in forest")
+            return False
+
+    for polygon in polygons_garden:
+        if point.Within(polygon):
+            print("in garden")
+            return False
+
+    for polygon in polygons_park:
+        if point.Within(polygon):
+            print("in park")
+            return False
+
+    for polygon in polygons_recreation_ground:
+        if point.Within(polygon):
+            print("in recreation ground")
+            return False
+
+    for polygon in polygons_water:
+        if point.Within(polygon):
+            print("in water")
+            return False
+
+    for polygon in polygons_wood:
+        if point.Within(polygon):
+            print("in wood")
+            return False
+
+    return True
 
 def get_coordinates(points):
     data = []
@@ -114,14 +189,26 @@ def write_coords_to_geojson(coords, file_path):
 
 NUM_POINTS = 10_000
 
-# Read berlin-inhabitants.geojson
-geojson = read_geojson('../data/inhabitants/berlin-inhabitants.geojson')
-
 # Get polygons
-polygons = get_polygons(geojson)
+polygons_districts = get_polygons(read_geojson('../data/inhabitants/berlin-inhabitants.geojson'))
+polygons_cemetery = get_polygons(read_geojson('../results/cemetery.geojson'))
+polygons_forest = get_polygons(read_geojson('../results/forest.geojson'))
+polygons_garden = get_polygons(read_geojson('../results/garden.geojson'))
+polygons_park = get_polygons(read_geojson('../results/park.geojson'))
+polygons_recreation_ground = get_polygons(read_geojson('../results/recreation_ground.geojson'))
+polygons_water = get_polygons(read_geojson('../results/water.geojson'))
+polygons_wood = get_polygons(read_geojson('../results/wood.geojson'))
 
 # Generate points in polygons
-points = get_random_points_in_polygons(polygons, NUM_POINTS)
+points = get_random_points_in_polygons(polygons_districts,
+                                       polygons_cemetery,
+                                       polygons_forest,
+                                       polygons_garden,
+                                       polygons_park,
+                                       polygons_recreation_ground,
+                                       polygons_water,
+                                       polygons_wood,
+                                       NUM_POINTS)
 
 # Get coordinates
 coords = get_coordinates(points)
